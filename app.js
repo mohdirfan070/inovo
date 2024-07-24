eb63b1cff6dfd9031be761daed4bb3975a827e5a
Mohammed Irfan <mohammedirfanrj@gmail.com>
2024-07-24 22:04:57 +0530

    Card Names

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 const byId =  eleId => document.getElementById(eleId);
 
 
-const card1  = byId('card-1-desktop');
-const card2  = byId('card-2-desktop');
-const card3  = byId('card-3-desktop');
-const card4  = byId('card-4-desktop');
-const card5  = byId('card-5-desktop');
 
-const navbar = byId('navbar');
